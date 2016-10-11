0e4204e01658f43be4a5d9a5a500500d593a87db
Daryuxz <deciimeedaroh@hotmail.com>
2016-10-11 20:08:40 -0300

    11-10

--- js/controllers/stockController.js
+++ js/controllers/stockController.js
@@ -1,8 +1,11 @@
 angular.module('stockApp', [])
 .controller('StockController', function($scope){
     $scope.products = [
-        {code:'0001', description:'IPhone 7', price:700, active:true, notes:'La esencia del iPhone a su máxima expresión', picture:'0001.png'},
-        {code:'0002', description:'Samsung Galaxy S7 Edge', price:650, active:true, notes:'Equilibrio perfecto entre forma y funcionalidad', picture:'0002.png'},
-        {code:'0003', description:'Microsoft 640 XL', price:350, active:true, notes:'Tu nuevo compañero de oficina móvil', picture:'0003.png'}
+        {code:'CP0001', description:'IPhone 7', price:700, active:true, notes:'La esencia del iPhone a su máxima expresión', picture:'CP0001.png'},
+        {code:'CP0002', description:'Samsung Galaxy S7 Edge', price:650, active:true, notes:'Equilibrio perfecto entre forma y funcionalidad', picture:'CP0002.png'},
+        {code:'CP0003', description:'Microsoft 640 XL', price:350, active:true, notes:'Tu nuevo compañero de oficina móvil', picture:'CP0003.png'},
+        {code:'PC0001', description:'Alien Ware', price:1900, active:true, notes:'Sistemas de alto rendimiento', picture:'PC0001.png'},
+        {code:'PC0002', description:'Asus G501', price:1500, active:true, notes:'La elección de los campeones', picture:'PC0002.png'}
+ 
     ];
 });
